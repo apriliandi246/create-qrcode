485d50dd556049c3fbea4c5439e8a8e676a56d42
Farhan Apriliandi <apriliandi642@gmail.com>
2021-02-21 17:57:38 +0700

    init project

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+   import { onMount } from "svelte";
+
+   let qrcode;
+
+   onMount(() => {
+      qrcode = new QRCode(document.getElementById("qrcode"), {
+         text: "https://github.com/apriliandi246",
+         width: 228,
+         height: 228,
+         colorDark: "#000000",
+         colorLight: "#ffffff",
+         correctLevel: QRCode.CorrectLevel.H,
+      });
+   });
+</script>
+
+<h1>Hello World</h1>
+
+<div id="qrcode" />
+
+<style>
+   h1 {
+      color: rgb(0, 102, 192);
+   }
+</style>
